ff114e6798ade8891167cc5653ef5cd8a881e255
Lei Li <leilioxford@gmail.com>
2020-07-28 22:12:53 -0500

    update

--- R/Visualization.R
+++ R/Visualization.R
@@ -1144,24 +1144,6 @@ SingleRasterMap <- function(
 #' @references https://adv-r.hadley.nz/functions.html#missing-arguments
 #'
 
-`%||%` <- function(lhs, rhs) {
-  if (!is.null(x = lhs)) {
-    return(lhs)
-  } else {
-    return(rhs)
-  }
-}
-
-#' Set a default value if an object is NOT null
-#'
-#' @param lhs An object to set if it's NOT null
-#' @param rhs The value to provide if x is NOT null
-#'
-#' @return lhs if lhs is null, else rhs
-#'
-#' @author Hadley Wickham
-#' @references https://adv-r.hadley.nz/functions.html#missing-arguments
-#'
 
 `%iff%` <- function(lhs, rhs) {
   if (!is.null(x = lhs)) {
